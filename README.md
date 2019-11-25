770a59f2ba8f19027822a9798fb83f7b63eede3f
Jason Gross <jgross@mit.edu>
2019-11-25 00:01:51 -0500

    Add pattern.v, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # coq-performance-tests
 A library of Coq source files testing for performance regressions on Coq
+
+## Contributing
+
+Please add tests to this repository.
+
+Each test should go in its own .v file, and each .v file should be
+targeted to take around 1 minute, so that all tests get roughly equal
+weight.
---
+++ pattern.v
@@ -0,0 +1,31 @@
+(** Testing the performance of [pattern].  For not regressing on COQBUG(https://github.com/coq/coq/issues/11150) and COQBUG(https://github.com/coq/coq/issues/6502) *)
+Definition Let_In {A P} (v : A) (f : forall x : A, P x) : P v
+:= let x := v in f x.
+
+Fixpoint big (a : nat) (sz : nat) : nat
+  := match sz with
+     | O => a
+     | S sz' => Let_In (a * a) (fun a' => big a' sz')
+     end.
+
+Ltac do_time n :=
+  try (
+      once (assert (exists e, e = big 1 n);
+            [ lazy -[big]; (*match goal with |- ?G => idtac G end;*) lazy [big];
+              time pattern Nat.mul, S, O, (@Let_In nat (fun _ => nat))
+            | ]);
+      fail).
+
+Local Set Warnings Append "-abstract-large-number".
+
+Goal True.
+Proof.
+  do_time 2; do_time 4; do_time 8; do_time 16; do_time 32; do_time 64;
+    do_time 128; do_time 256; do_time 512.
+  idtac 1024; do 9 do_time 1024.
+  idtac 2048; do 9 do_time 2048.
+  idtac 4096; do 9 do_time 4096.
+  idtac 8192; do 9 do_time 8192.
+  idtac 16384; do 9 do_time 16384.
+  exact I.
+Defined.
