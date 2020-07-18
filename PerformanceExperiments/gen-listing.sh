24dede716f3c4ea5fc58ad0228e654ac643b0f27
Jason Gross <jgross@mit.edu>
2020-07-18 14:56:39 -0400

    Remove typod "

--- PerformanceExperiments/gen-listing.sh
+++ PerformanceExperiments/gen-listing.sh
@@ -19,5 +19,5 @@ for stem in $stems; do
     echo -n '  '
     echo $coq_versions | sed 's/[^ ]\+/--/g; s/ /|/g; s/$/'"$extra_bar"'/g'
     echo -n '  '
-    echo $coq_versions | sed 's/ / | /g; s,\([^ |]\+\),<a href="https://coq-community.github.io/coq-performance-tests/\1/'"${stem_dash}"'.svg">"<img src="https://coq-community.github.io/coq-performance-tests/\1/'"${stem_dash}"'.svg" height=100px /></a>,g; s/$/'"$extra_bar_space"'/g'
+    echo $coq_versions | sed 's/ / | /g; s,\([^ |]\+\),<a href="https://coq-community.github.io/coq-performance-tests/\1/'"${stem_dash}"'.svg"><img src="https://coq-community.github.io/coq-performance-tests/\1/'"${stem_dash}"'.svg" height=100px /></a>,g; s/$/'"$extra_bar_space"'/g'
 done
