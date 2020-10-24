647263b85437c6cb1a89ed7f792b1f6e2b44287e
Jason Gross <jgross@mit.edu>
2020-10-24 16:29:01 -0400

    Finish setting up plots

--- plots/generate-tex.py
+++ plots/generate-tex.py
@@ -5,6 +5,7 @@ COLORS = 'red green blue cyan magenta yellow black gray brown lime olive orange 
 MARKS = 'o asterisk star oplus otimes square square* triangle triangle* diamond diamond* pentagon pentagon* - | oplus* otimes*'.split(' ')
 
 SKIP_SUFFIXES = ('-sys', '-user')
+SEPARATOR = ','
 
 def to_valid_tex_cmd(s):
     numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
@@ -46,7 +47,7 @@ def generate_legend_and_regression(color, mark, xlabel, ylabel, name):
         variables_str = '\n'.join(fr'        \expandafter\xdef\csname pgfplotstable{short_ylabel}-{regression_kind}regression{v}\endcsname{{\pgfplotstableregression{v}}}' for v in variables[regression_kind])
         if regression_kind == 'linear':
             ret += fr'''
-        \addplot[no markers, color={color}] table[x=param-{xlabel},y={{create col/linear regression={{y={ylabel}}}}}]{{{name}.txt}};
+        \addplot[no markers, color={color}] table[x=param-{xlabel},y={{create col/linear regression={{y={ylabel}}}}},col sep=comma]{{{name}.txt}};
 {variables_str}
         \addlegendentry{{${print_poly(regression_kind)}$}}'''
         elif regression_kind in ('quadratic', 'cubic', 'exponential'):
@@ -63,19 +64,66 @@ def generate_legend_and_regression(color, mark, xlabel, ylabel, name):
             raise Exception('Invalid regression_kind: %s (not in %s)' % (regression_kind, ', '.join(sorted(variables.keys()))))
     return ret
 
+def maxdict(d):
+    ret = {}
+    for k, v in d:
+        if k in ret.keys(): ret[k] = max(ret[k], v)
+        else: ret[k] = v
+    return ret
+
+def generate_keys(txt_lines):
+    rows = [row.strip().split(SEPARATOR) for row in txt_lines]
+    col_count = max(map(len, rows))
+    def get_from_row(row, c):
+        if c < len(row):
+            try:
+                if '.' not in row[c]:
+                    return int(row[c])
+                else:
+                    return float(row[c])
+            except:
+                return row[c]
+        return None
+    cols = [[get_from_row(row, i) for row in rows]
+            for i in range(col_count)]
+    funcs = dict((col[0], maxdict((cols[0][i], col[i]) for i in range(1, len(col)) if isinstance(col[i], float) or isinstance(col[i], int)))
+                 for col in cols[1:])
+    def approximate(k, n):
+        if n in funcs[k].keys(): return funcs[k][n]
+        if min(funcs[k].keys()) <= n and n <= max(funcs[k].keys()):
+            x1 = max(nv for nv in funcs[k].keys() if nv <= n)
+            x2 = min(nv for nv in funcs[k].keys() if n <= nv)
+        elif n < min(funcs[k].keys()):
+            x1, x2 = sorted(funcs[k].keys())[:2]
+        elif max(funcs[k].keys()) < n:
+            x1, x2 = sorted(funcs[k].keys())[-2:]
+        else:
+            assert(False)
+        y1, y2 = funcs[k][x1], funcs[k][x2]
+        # y1 = m * x1 + b
+        # y2 = m * x2 + b
+        # m = (y2 - y1) / (x2 - x1)
+        # b = y1 - m * x1
+        m = float(y2 - y1) / float(x2 - x1)
+        b = y1 - m * x1
+        return m * n + b
+    x = max(v for v in cols[0] if isinstance(v, int) or isinstance(v, float))
+    return dict((k, approximate(k, x)) for k in rows[0][1:])
+
 def generate_tex(name, txt_lines):
-    header = txt_lines[0].strip().split(' ')
+    header = txt_lines[0].strip().split(SEPARATOR)
     ylabels = [h for h in header[1:] if not h.startswith('param-')]
-    cols = [line.strip().split(' ')[i] for line in txt_lines[1:] for i in range(len(header))]
+    cols = [line.strip().split(SEPARATOR)[i] for line in txt_lines[1:] for i in range(len(header))]
     extra_params_descr = ', '.join(u"%s \u2208 {%s}" % (h, ', '.join(sorted(set(col)))) for h, col in zip(header[1:], cols[1:]) if h.startswith('param-'))
     if extra_params_descr != '': extra_params_descr = ' ' + extra_params_descr
-    ylabels_dict = {ylabel:float(val) for ylabel, val in zip(txt_lines[0].strip().split(' '), txt_lines[-1].strip().split(' '))}
+    #ylabels_dict = {ylabel:float(val) for ylabel, val in zip(txt_lines[0].strip().split(SEPARATOR), txt_lines[-1].strip().split(SEPARATOR))}
+    ylabels_dict = generate_keys(txt_lines)
     contents = ''.join(txt_lines)
     short_name = to_valid_tex_cmd(name)
     if len(ylabels) < 1: raise Exception('Invalid header with not enough columns: %s' % repr(txt_lines[0]))
     if not header[0].startswith('param-'): raise Exception('Invalid header: first column %s does not start with param-' % header[0])
     xlabel = header[0][len('param-'):]
-    plots = [fr'        \addplot[only marks,mark={mark},color={color}] table[x=param-{xlabel},y={ylabel}]{{{name}.txt}};'
+    plots = [fr'        \addplot[only marks,mark={mark},color={color}] table[x=param-{xlabel},y={ylabel},col sep=comma]{{{name}.txt}};'
              + generate_legend_and_regression(color=color, mark=mark, xlabel=xlabel, ylabel=ylabel, name=name)
              for mark, color, ylabel
              in zip(itertools.cycle(MARKS),
