03eb472d39c27e66ee02cf9b5c35961a753738f8
Jason Gross <jgross@mit.edu>
2022-02-02 11:42:05 -0500

    Add evars in rewriting test

--- PerformanceExperiments/rewrite_repeated_app_ssrrewrite_noop.v
+++ PerformanceExperiments/rewrite_repeated_app_ssrrewrite_noop.v
@@ -16,12 +16,18 @@ Require Import Coq.ssr.ssreflect.
 Ltac do_ssr_rewrite_bang := try rewrite !f'g'.
 Ltac do_ssr_rewrite_once := try rewrite f'g'.
 Ltac do_ssr_rewrite_ques := try rewrite ?f'g'.
+Ltac do_ssr_rewrite_bang_evar := try rewrite !(f'g' _).
+Ltac do_ssr_rewrite_once_evar := try rewrite (f'g' _).
+Ltac do_ssr_rewrite_ques_evar := try rewrite ?(f'g' _).
 
 Ltac time_solve_goal0 n :=
   time "try-ssr-rewrite!-cold-noop-regression-linear" do_ssr_rewrite_bang;
   time "try-ssr-rewrite!-noop-regression-linear" do_ssr_rewrite_bang;
   time "try-ssr-rewrite-noop-regression-linear" do_ssr_rewrite_once;
   time "try-ssr-rewrite?-noop-regression-linear" do_ssr_rewrite_ques;
+  time "try-ssr-rewrite!-evar-noop-regression-linear" do_ssr_rewrite_bang_evar;
+  time "try-ssr-rewrite-evar-noop-regression-linear" do_ssr_rewrite_once_evar;
+  time "try-ssr-rewrite?-evar-noop-regression-linear" do_ssr_rewrite_ques_evar;
   reflexivity.
 
 Ltac run0 sz := Harness.runtests args_of_size describe_goal_noop_exp mkgoal_noop_exp redgoal time_solve_goal0 sz.
