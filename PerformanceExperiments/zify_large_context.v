394e049bfa3da6f2c2d51d75a8ef0c1ebd517bdc
Jason Gross <jgross@mit.edu>
2024-03-12 12:49:10 -0700

    Regression line for `zify_large_context`
    Adapt #42 to the plotting infrastructure

---
+++ PerformanceExperiments/zify_large_context.v
@@ -0,0 +1,31 @@
+(** Testing the performance of [zify] and [Z.to_euclidean_division_equations].
+For not regressing on COQBUG(https://github.com/coq/coq/issues/18770) *)
+From Coq Require Import ZArith List Zify.
+Import ListNotations.
+Require Import PerformanceExperiments.Harness.
+
+Global Open Scope Z_scope.
+
+Ltac Zify.zify_post_hook ::= Z.to_euclidean_division_equations.
+
+Ltac mkgoal n := constr:(fold_left (fun T _ => (forall x, 0 <= x < 100 -> T)) (repeat tt (Z.to_nat n)) True).
+Ltac redgoal _ := lazy -[Z.le Z.lt]; intros.
+
+Ltac time_solve_goal0 n :=
+  time "zify-big-context-regression-quadratic" zify.
+
+Definition args_of_size (s : size) : list Z
+  := match s with
+     | Sanity => List.map Z.of_nat (seq 1 3)
+     | SuperFast => List.map Z.of_nat (seq 1 140)
+     | Fast => List.map (fun x => Z.of_nat x * 100) (seq 1 10) ++ [1500; 2000]
+     | Medium => []
+     | Slow => []
+     | VerySlow => []
+     end.
+
+Ltac run0 sz := Harness.runtests args_of_size default_describe_goal mkgoal redgoal time_solve_goal0 sz.
+
+(*
+Goal True. Time run0 Fast.
+*)
